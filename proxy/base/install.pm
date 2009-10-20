72b3486b6e8477a8b04aae350ea05a747681beb4
Stephane Alnet <stephane@shimaore.net>
2009-10-21 01:26:08 +0200

    First install.pm modules

--- proxy/base/install.pm
+++ proxy/base/install.pm
@@ -1,5 +1,6 @@
 
-my ($hostname,$domain_name,$cluster_name,$role,$function) = @ARGV;
+use constant proxy_mode => 'proxy_mode';
+use constant proxy_mode_file => File::Spec->catfile(CCN,proxy_mode);
 
 # MODEL: Which model is used for the local opensips system
 # Must be onre of the *.recipe names:
@@ -10,16 +11,21 @@ my ($hostname,$domain_name,$cluster_name,$role,$function) = @ARGV;
 #    registrar
 #    router-no-registrar
 #    router
-export MODEL=complete-transparent
 
-# end of configuration parameters.
+sub run {
+  my $model = first_line_of(proxy_mode_file);
 
-# Generate a new opensips.cfg and opensips.sql file and push them
-echo "Generating a new opensips.cfg and opensips.sql"
-(cd $SRC/base/opensips && mkdir -p output && ./build.sh $MODEL && mv output/opensips.* $WWW/CCNQ/Proxy/templates)
-# Reconfigure the local system (includes installing the new opensips.cfg file in /etc/opensips)
-echo "Reconfiguring the local system"
-(cd $WWW && sudo perl CCNQ/Proxy/bin/configure.pl)
-# Restart OpenSIPS using the new configuration.
-echo "Restarting OpenSIPS"
-sudo /etc/init.d/opensips restart
+=pod
+  # Generate a new opensips.cfg and opensips.sql file and push them
+  echo "Generating a new opensips.cfg and opensips.sql"
+  (cd $SRC/base/opensips && mkdir -p output && ./build.sh $MODEL && mv output/opensips.* $WWW/CCNQ/Proxy/templates)
+  # Reconfigure the local system (includes installing the new opensips.cfg file in /etc/opensips)
+  echo "Reconfiguring the local system"
+  (cd $WWW && sudo perl CCNQ/Proxy/bin/configure.pl)
+  # Restart OpenSIPS using the new configuration.
+  echo "Restarting OpenSIPS"
+  sudo /etc/init.d/opensips restart
+=cut
+
+}
+
---
+++ proxy/inbound-proxy/install.pm
@@ -0,0 +1,4 @@
+use constant proxy_mode => 'proxy_mode';
+use constant proxy_mode_file => File::Spec->catfile(CCN,proxy_mode);
+
+sub run { print_to(proxy_mode_file,'inbound-proxy'); }
