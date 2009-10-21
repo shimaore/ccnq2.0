b1e73121d7882c34eb0fa9f974278a5154fc451d
Stephane Alnet <stephane@shimaore.net>
2009-10-21 17:26:11 +0200

    Testing

---
+++ proxy/inbound-proxy/actions.pm
@@ -0,0 +1,8 @@
+{
+  install => sub {
+    use constant proxy_mode => 'proxy_mode';
+    use constant proxy_mode_file => File::Spec->catfile(CCN,proxy_mode);
+
+    print_to(proxy_mode_file,'inbound-proxy');
+  },
+}
--- proxy/inbound-proxy/install.pm
+++
@@ -1,4 +0,0 @@
-use constant proxy_mode => 'proxy_mode';
-use constant proxy_mode_file => File::Spec->catfile(CCN,proxy_mode);
-
-sub run { print_to(proxy_mode_file,'inbound-proxy'); }
