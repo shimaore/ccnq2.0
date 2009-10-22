a8e5af973d461f8cf651fbae721085af6d6f5cfc
Stephane Alnet <stephane@shimaore.net>
2009-10-22 22:25:14 +0200

    Testing MUC

--- node/api/actions.pm
+++ node/api/actions.pm
@@ -13,10 +13,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use JSON;
+use AnyEvent::HTTPD;
+
 {
   _session_ready => {
-    use JSON;
-    use AnyEvent::HTTPD;
 
     our ($context) = @_;
 
