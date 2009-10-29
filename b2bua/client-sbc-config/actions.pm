772907341424b933892db21577e61e0f5f74b584
Stephane Alnet <stephane@shimaore.net>
2009-10-29 14:51:16 +0100

    SBC installers

---
+++ b2bua/client-sbc-config/actions.pm
@@ -0,0 +1,17 @@
+use CCNQ::B2BUA;
+
+use constant base_name => 'client-sbc-config';
+
+{
+  install => sub {
+    # dialplan
+    for my $name in qw( plain ) {
+      CCNQ::B2BUA::copy_file(base_name,qw( dialplan ),"${name}.xml");
+    }
+
+    # sip_profile
+    for my $name in qw( plain ) {
+      CCNQ::B2BUA::copy_file(base_name,qw( sip_profiles ),"${name}.xml");
+    }
+  },
+}
