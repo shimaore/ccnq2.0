7ddb2d7602170b6e87f76e95ed778af99146d13f
Stephane Alnet <stephane@shimaore.net>
2009-11-05 11:40:05 +0100

    Installation for plain-cnam.

--- b2bua/client-sbc-config/actions.pm
+++ b2bua/client-sbc-config/actions.pm
@@ -5,13 +5,23 @@ use CCNQ::B2BUA;
     my $b2bua_name = 'client-sbc-config';
 
     # dialplan
-    for my $name (qw( plain )) {
+    for my $name (qw( plain plain-cnam )) {
       CCNQ::B2BUA::copy_file($b2bua_name,qw( dialplan ),"${name}.xml");
     }
 
+    # dialplan/template
+    for my $name (qw( plain plain-cnam )) {
+      CCNQ::B2BUA::copy_file($b2bua_name,qw( dialplan template ),"${name}.xml");
+    }
+
     # sip_profile
-    for my $name (qw( plain )) {
+    for my $name (qw( plain plain-cnam )) {
       CCNQ::B2BUA::copy_file($b2bua_name,qw( sip_profiles ),"${name}.xml");
+    }
+
+    # scripts
+    for my $name (qw( cnam.pl )) {
+      CCNQ::B2BUA::copy_file($b2bua_name,qw( scripts ),${name});
     }
   },
 }
