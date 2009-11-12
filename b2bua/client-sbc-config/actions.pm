0ea632871860974c1de7140ce5aa22c06b9bf8b8
Stephane Alnet <stephane@shimaore.net>
2009-11-12 14:10:02 +0100

    Added transparent SBC client profile; updated installer

--- b2bua/client-sbc-config/actions.pm
+++ b2bua/client-sbc-config/actions.pm
@@ -5,22 +5,23 @@ use CCNQ::B2BUA;
     my $b2bua_name = 'client-sbc-config';
 
     # acls
-    for my $name (qw( client-sbc-config )) {
+    for my $name ($b2bua_name) {
       CCNQ::B2BUA::copy_file($b2bua_name,qw( autoload_configs ),"${name}.acl.xml");
     }
 
     # dialplan
-    for my $name (qw( all )) {
+    for my $name ($b2bua_name) {
       CCNQ::B2BUA::copy_file($b2bua_name,qw( dialplan ),"${name}.xml");
     }
 
     # dialplan/template
-    for my $name (qw( client-sbc-template france-egress france-ingress usa-cnam-egress usa-cnam-ingress usa-egress usa-ingress )) {
+    for my $name (qw( client-sbc-template ),
+        map {($_.'-ingress',$_.'-egress')} qw( e164 france loopback transparent usa-cnam usa )) {
       CCNQ::B2BUA::copy_file($b2bua_name,qw( dialplan template ),"${name}.xml");
     }
 
     # sip_profile
-    for my $name (qw( usa usa-cnam france e164 )) {
+    for my $name ($b2bua_name) {
       CCNQ::B2BUA::copy_file($b2bua_name,qw( sip_profiles ),"${name}.xml");
     }
 
