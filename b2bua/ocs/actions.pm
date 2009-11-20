816b9a142a8d07c03b62d144cb9eac733d6d9d35
Stephane Alnet <stephane@shimaore.net>
2009-11-21 00:18:55 +0100

    b2bua/ocs

---
+++ b2bua/ocs/actions.pm
@@ -0,0 +1,48 @@
+# client-sbc-config/actions.pm
+
+# Copyright (C) 2009  Stephane Alnet
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use CCNQ::B2BUA;
+
+{
+  install => sub {
+    my ($params,$context,$mcv) = @_;
+
+    my $b2bua_name = 'ocs';
+
+    # acls
+    for my $name ($b2bua_name) {
+      CCNQ::B2BUA::copy_file($b2bua_name,qw( autoload_configs ),"${name}.acl.xml");
+    }
+
+    # dialplan
+    for my $name ($b2bua_name) {
+      CCNQ::B2BUA::copy_file($b2bua_name,qw( dialplan ),"${name}.xml");
+    }
+
+    # sip_profile
+    for my $name ($b2bua_name) {
+      CCNQ::B2BUA::copy_file($b2bua_name,qw( sip_profiles ),"${name}.xml");
+    }
+
+    # scripts
+    for my $name (qw( cnam.pl )) {
+      CCNQ::B2BUA::copy_file($b2bua_name,qw( .. scripts ),${name});
+    }
+
+    $mcv->send(CCNQ::Install::SUCCESS);
+  },
+}
---
+++ b2bua/ocs/freeswitch/scripts/cnam.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+use strict; use warnings;
+our $session;
+
+use constant CNAM_URI => 'https://cnam.sotelips.net:9443/callingname.yaws?Number=';
+
+sub info
+{
+  freeswitch::consoleLog("info", "CNAM: ".join(' ',@_)."\n");
+}
+
+sub cnam_run {
+
+  info('Starting');
+  my $cid = $session->getVariable('caller_id_number');
+  info('CID:',$cid);
+  if( $cid =~ /^[+]?1?(\d{10})$/ )
+  {
+    my $number = $1;
+    info('Number:',$number);
+
+    use LWP::UserAgent;
+    my $ua = LWP::UserAgent->new;
+    my $req = HTTP::Request->new(
+      POST => CNAM_URI.$number
+    );
+
+    my $res = $ua->request($req);
+
+    if($res->is_success) {
+      my $string = $res->content;
+      $string =~ s/[^\w,.@-]/ /g;
+      $string =~ s/\s+$//g;
+      $session->setVariable('effective_caller_id_name',$string);
+      info('Got calling name:',$string);
+    }
+    else
+    {
+      info('Error:',$res->status_line);
+      $session->setVariable('effective_caller_id_name','No data');
+    }
+  }
+  else
+  {
+    info('Improper CID format',$cid);
+  }
+  info('Done');
+}
+
+cnam_run();
+1;
