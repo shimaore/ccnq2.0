d912e1b410912b08ef257877a39ecd17202ee3de
Stephane Alnet <stephane@shimaore.net>
2009-12-04 14:29:20 +0100

    Fixing includes

--- node/actions.pm
+++ node/actions.pm
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use Carp;
+
 {
   install_all => sub {
     my ($params,$context,$mcv) = @_;
