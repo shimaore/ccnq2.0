e3ca2911cd4ef54c98d31dbed302bd26c9dc37ff
Stephane Alnet <stephane@shimaore.net>
2010-02-08 14:09:59 +0100

    (moved) metadat into CCNQ.pm

--- common/perllib/lib/CCNQ.pm
+++ common/perllib/lib/CCNQ.pm
@@ -1,20 +1,34 @@
 package CCNQ;
-# Copyright (C) 2009  Stephane Alnet
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Currently this module only exists to make File::ShareDir happy.
-# Eventually most of the CCNQ::Install code should probably be migrated here.
+=head1 NAME
+  Distribution for ccnq2.0
+
+=head1 AUTHOR
+  Stephane Alnet <stephane@shimaore.net>
+
+=head1 LICENSE
+ Copyright (C) 2009  Stephane Alnet
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 3
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ Currently this module only exists to make File::ShareDir happy.
+ Eventually most of the CCNQ::Install code should probably be migrated here.
+
+=cut
+
+use 5.008;
+
+our $VERSION = '0.02';
 
 1;
