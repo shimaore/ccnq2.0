35ecb4c3f9b01d4c3c8bb9f10d1269f4f1fda8bc
Stephane Alnet <stephane@shimaore.net>
2010-02-11 12:16:28 +0100

    (change) install contrib/manage scripts in /usr/bin

--- contrib/install-manage.sh
+++ contrib/install-manage.sh
@@ -1,9 +1,9 @@
 mkdir -p /etc/ccn
 [ -e /etc/ccn/servers ] || cp manage/etc/ccn/servers /etc/ccn/servers
 
-(cd manage/usr/sbin;         \
+(cd manage/usr/bin;         \
   for f in ccnq2*; do        \
-    cp $f /usr/sbin/$f;      \
-    chmod +x /usr/sbin/$f;   \
+    cp $f /usr/bin/$f;      \
+    chmod +x /usr/bin/$f;   \
   done )
 
