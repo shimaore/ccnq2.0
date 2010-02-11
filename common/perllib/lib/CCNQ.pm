26adcd8ba6aafb711d10cdd9d18856b58b1e5d97
Stephane Alnet <stephane@shimaore.net>
2010-02-11 10:18:43 +0100

    (packaging) trying to make Module::Install autodetect

--- common/perllib/lib/CCNQ.pm
+++ common/perllib/lib/CCNQ.pm
@@ -1,29 +1,34 @@
 package CCNQ;
 
 =head1 NAME
-  Distribution for ccnq2.0
+
+Distribution for ccnq2.0
+
+=head1 SYNOPSIS
+
+Currently this module only exists to make File::ShareDir happy.
+Eventually most of the CCNQ::Install code should probably be migrated here.
 
 =head1 AUTHOR
-  Stephane Alnet <stephane@shimaore.net>
+
+Stephane Alnet <stephane@shimaore.net>
 
 =head1 LICENSE
- Copyright (C) 2009  Stephane Alnet
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 3
- of the License, or (at your option) any later version.
+Copyright (C) 2009  Stephane Alnet
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- Currently this module only exists to make File::ShareDir happy.
- Eventually most of the CCNQ::Install code should probably be migrated here.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 
