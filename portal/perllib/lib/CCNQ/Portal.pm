6fa6da7cb6d0a09aa3901af518790c2bd895d767
Stephane Alnet <stephane@shimaore.net>
2010-02-21 22:18:00 +0100

    (minor,change) Portal license

--- portal/perllib/lib/CCNQ/Portal.pm
+++ portal/perllib/lib/CCNQ/Portal.pm
@@ -6,23 +6,20 @@ package CCNQ::Portal;
   Stephane Alnet <stephane@shimaore.net>
 
 =head1 LICENSE
- Copyright (C) 2009  Stephane Alnet
+Copyright (C) 2009  Stephane Alnet
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 3
- of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- Currently this module only exists to make File::ShareDir happy.
- Eventually most of the CCNQ::Install code should probably be migrated here.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 
@@ -31,7 +28,6 @@ use 5.008;
 our $VERSION = '0.02';
 
 use strict; use warnings;
-use CCNQ::Portal::Session;
 
 sub new {
   my $this = shift; my $class = ref($this) || $this;
@@ -45,6 +41,7 @@ our $site;
 # e.g.   CCNQ::Portal->set_site(CCNQ::Portal::Site->new( default_locale => 'en-US', security => new CCNQ::Portal::Auth::LDAP( ... ) )
 
 sub set_site {
+  my $self = shift;
   $site = shift;
 }
 
@@ -52,10 +49,13 @@ sub site {
   return $site;
 }
 
+use CCNQ::Portal::Session;
+
 our $session;
 
 sub current_session {
-  return $session ||= new CCNQ::Portal::Session;
+  my $self = shift;
+  return $session ||= CCNQ::Portal::Session->new($self->site);
 }
 
 'CCNQ::Portal';
