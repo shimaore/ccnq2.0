2f18cac7cc16b9ef7f09f792c20cff8df5551575
Stephane Alnet <stephane@shimaore.net>
2010-02-21 20:43:24 +0100

    (fix) Portal::Session

--- portal/perllib/lib/CCNQ/Portal/Session.pm
+++ portal/perllib/lib/CCNQ/Portal/Session.pm
@@ -10,6 +10,7 @@ sub new {
 }
 
 sub start {
+  my $self = shift;
   session user_id => shift;
   # XXX Should be configurable, and able to say "+15m".
   session expires => time() + 15 * 60;
@@ -18,24 +19,27 @@ sub start {
 }
 
 sub end {
+  my $self = shift;
   session user_id => undef;
   session expires => undef;
   # Keep the user's locale.
 }
 
 sub user {
+  my $self = shift;
   # Make sure the session hasn't expired.
   return undef if session('expires') && session('expires') > time();
   # Return the proper user object.
-  return session('user_id') && new CCNQ::Portal::User(session('user_id')); 
+  return session('user_id') && new CCNQ::Portal::User(session('user_id'));
 }
 
 sub locale {
+  my $self = shift;
   # Try to automatically select a locale if none has been chosen.
   if(!session->('locale')) {
-    session locale => 
+    session locale =>
       # Use the user's preferred locale if one is available.
-        (CCNQ::Portal::current_user && CCNQ::Portal::current_user->default_locale)
+        ($self->user && $self->user->default_locale)
       # XXX Use the browser's preferred locales!
       # Otherwise default to the site's preferred locale.
       || CCNQ::Portal::site->default_locale;
