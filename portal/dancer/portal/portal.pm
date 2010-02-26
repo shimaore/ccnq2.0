13b3c98dd79a9e9f8e77231a178abc0969353476
Stephane Alnet <stephane@shimaore.net>
2010-02-26 18:23:07 +0100

    "pass" only works on same Method

--- portal/dancer/portal/portal.pm
+++ portal/dancer/portal/portal.pm
@@ -23,7 +23,7 @@ use CCNQ::Portal::Outer::AccountSelection;
 #     }
 # };
 
-any [ 'get', 'post' ] '/' => sub {
+any [ 'get', 'post' ] => '/' => sub {
   my $template_name = 'index';
   $template_name = 'result' if vars->{result};
   my $vars = vars;
