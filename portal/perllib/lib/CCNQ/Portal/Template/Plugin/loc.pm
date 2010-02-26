6a06483dc4de9dac0091671ec5fe40c9f687456a
Stephane Alnet <stephane@shimaore.net>
2010-02-26 01:55:33 +0100

    (fix,template) loc

--- portal/perllib/lib/CCNQ/Portal/Template/Plugin/loc.pm
+++ portal/perllib/lib/CCNQ/Portal/Template/Plugin/loc.pm
@@ -6,6 +6,7 @@ use CCNQ::Portal;
 
 sub init {
     my $self = shift;
+    $self->{ _DYNAMIC } = 1;
     my $name = $self->{ _CONFIG }->{ name } || 'loc';
     $self->install_filter($name);
     return $self;
@@ -14,14 +15,30 @@ sub init {
 sub filter {
     my ($self, $text) = @_;
 
-    return CCNQ::Portal->current_session->locale->loc($text);
+    my $args = $self->{_ARGS};
+    $text = CCNQ::Portal->current_session->locale->loc($text,@{$args});
+    for ($text) {
+        s/&/&amp;/g;
+        s/</&lt;/g;
+        s/>/&gt;/g;
+        s/"/&quot;/g;
+    }
+    return $text;
 }
 
-=head1 loc Filter
+=head1
+Internationalization Filter for Template-Toolkit.
+
+This filter also does html expansion (like the default "html" filter),
+since apparently custom filters cannot be part of a chain of filters.
+
+This is therefor suitable for use in HTML content and values.
 
 Use as follows:
 
     <% | loc %>String to be localized<% END %>
+
+    <% | loc param1 %>String with param [_1]<% END %>
 
 =cut
 
