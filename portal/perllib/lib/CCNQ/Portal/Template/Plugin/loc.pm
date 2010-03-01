9c9522d6eca82111ad6f02239352aaaee74cc2b8
Stephane Alnet <stephane@shimaore.net>
2010-03-01 11:52:16 +0100

    (revert)

--- portal/perllib/lib/CCNQ/Portal/Template/Plugin/loc.pm
+++ portal/perllib/lib/CCNQ/Portal/Template/Plugin/loc.pm
@@ -27,7 +27,7 @@ sub filter {
 
     # Template.pm doesn't do further processing, and Dancer (like LWP) expects
     # an encoded (binary) stream.
-    # $text = encode_utf8($text);
+    $text = encode_utf8($text);
     return $text;
 }
 
