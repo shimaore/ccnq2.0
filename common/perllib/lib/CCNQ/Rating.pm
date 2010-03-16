c9458ea1c99f5ca8c9671ccacc4b72e396d0e900
Stephane Alnet <stephane@shimaore.net>
2010-03-16 11:45:15 +0100

    (minor) notes on rounding -- need a new abstraction to differentiate post-paid billing vs pre-paid billing vs realtime (pre-paid) updates

--- common/perllib/lib/CCNQ/Rating.pm
+++ common/perllib/lib/CCNQ/Rating.pm
@@ -36,6 +36,15 @@ sub rate_and_save_cbef {
 
   my $rated_cbef = CCNQ::Rating::Rate::rate_cbef($cbef,$plan);
 
+  # XXX At this point we should round-up the computed (per-CBEF)
+  #     costs to (e.g.) 2 decimals, before applying taxes.
+  #     However, if we do post-paid billing, we might want to carryover
+  #     to more decimals (e.g. 4) as a business practice.
+  #     (In pre-paid situations, we will need to compute the taxes as we go;
+  #     in post-paid situations, they are generally computed on the total amount,
+  #     not on a per-line basis.)
+  $cbef->{taxable_cost} = $cbef->cost;
+
   # For each tax jurisdiction, compute the amount owed
   # Note: this is applied to the total cost, we don't
   #       know (yet) how to differentiate tax rates on
@@ -43,13 +52,15 @@ sub rate_and_save_cbef {
   for my $tax (@{$cbef->tax || []}) {
     for my $jurisdiction (keys %{$tax}) {
       my $rate = $tax->{$jurisdiction};
-      my $tax_amount = $cbef->rounding($cbef->cost * ($rate/100.0));
+      my $tax_amount = $cbef->rounding($cbef->taxable_cost * ($rate/100.0));
       $cbef->{taxes}->{$jurisdiction} += $tax_amount;
       $cbef->{tax_amount} += $tax_amount;
     }
   }
+
   # Then compute the grand-total cost of the call (including taxes)
-  $cbef->{total_cost} = $cbef->cost + $cbef->tax_amount;
+  # XXX Again, as above, this might need rounding up.
+  $cbef->{total_cost} = $cbef->taxable_cost + $cbef->tax_amount;
 
   # Save the new (rated) CBEF...
   CCNQ::Rating::Event::save_rated_cbef($rated_cbef);
