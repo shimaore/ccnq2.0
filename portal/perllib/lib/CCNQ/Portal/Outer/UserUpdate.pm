55695aeb467406f929718d5b8e5d6f536b55afe1
Stephane Alnet <stephane@shimaore.net>
2010-04-08 00:01:41 +0200

    (fix) user_update

--- portal/perllib/lib/CCNQ/Portal/Outer/UserUpdate.pm
+++ portal/perllib/lib/CCNQ/Portal/Outer/UserUpdate.pm
@@ -61,10 +61,10 @@ sub update {
   if( $user_id eq CCNQ::Portal->current_session->user->id ||
       CCNQ::Portal->current_session->user->profile->is_admin ) {
     # Name
-    $params->{name} = $untainter->extract(-as_printable=>params->{name});
+    $params->{name} = $untainter->extract(-as_printable=>'name');
 
     # Email address
-    my $email = $untainter->extract(-as_email=>params->{email});
+    my $email = $untainter->extract(-as_email=>'email');
     if($email) {
       $params->{email} = $email->format;
     } else {
