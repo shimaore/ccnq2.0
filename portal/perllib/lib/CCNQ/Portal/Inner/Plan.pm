34b296b8b99cf77825b9e6fa0812eae9b4e1be20
Stephane Alnet <stephane@shimaore.net>
2010-04-20 16:52:24 +0200

    Corrected plan UI

--- portal/perllib/lib/CCNQ/Portal/Inner/Plan.pm
+++ portal/perllib/lib/CCNQ/Portal/Inner/Plan.pm
@@ -14,6 +14,7 @@ package CCNQ::Portal::Inner::Plan;
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 use strict; use warnings;
+use utf8;
 
 use Dancer ':syntax';
 use CCNQ::Portal;
