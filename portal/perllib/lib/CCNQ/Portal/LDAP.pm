9661c1a711d4ee8d49d2de316b3f5f9136e2f180
Stephane Alnet <stephane@shimaore.net>
2010-04-26 12:10:14 +0200

    Updated CCNQ::Portal::LDAP so that it is self-contained

--- portal/perllib/lib/CCNQ/Portal/LDAP.pm
+++ portal/perllib/lib/CCNQ/Portal/LDAP.pm
@@ -15,41 +15,31 @@ package CCNQ::Portal::LDAP;
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 use strict; use warnings;
 
+use CCNQ;
+
 use Net::LDAP;
 
-use constant URL_ERROR    => 'https://sotelips.net/d/?q=node/63';
-use constant LDAP_BIND    => 'cn=admin,dc=sotelips,dc=net';
-use constant LDAP_BASE    => 'ou=Users,dc=sotelips,dc=net';
-
-use constant LDAP_URI     => 'ldaps://ldap.sotelips.net';
-
-sub _error {
-  my $cgi = shift;
-  warn(@_);
-  print($cgi->redirect(URL_ERROR));
-  exit 0;
-}
+use Syslog::Logger;
 
 #
 ## Open a link to the LDAP store
 #
 sub get_ldap
 {
-  my $cgi = shift;
-  open(my $pass_fh, '<', '/etc/ccn/ldap.pass') or _error($cgi,$!);
+  my ($ldap_uri,$ldap_bind) = @_;
+  open(my $pass_fh, '<', CCNQ::CCN.'/ldap.pass') or error($!);
   my $ldap_password = <$pass_fh>;
   chomp $ldap_password;
-  close($pass_fh) or _error($cgi,$!);
+  close($pass_fh) or error($!);
 
-  my $ldap = Net::LDAP->new( LDAP_URI, timeout => 5 ) or _error($cgi,$!);
-  my $mesg = $ldap->bind( LDAP_BIND, password => $ldap_password );
-  _error($cgi,$mesg->error) if $mesg->code;
+  my $ldap = Net::LDAP->new( $ldap_uri, timeout => 5 ) or _error($cgi,$!);
+  my $mesg = $ldap->bind( $ldap_bind, password => $ldap_password );
+  error($mesg->error) if $mesg->code;
   return $ldap;
 }
 
 sub get_sn
 {
-  my $cgi = shift;
   my ($ldap,$email) = @_;
   my $mesg = $ldap->search(
     base => LDAP_BASE,
@@ -57,7 +47,7 @@ sub get_sn
     attrs => ['sn'],
     filter => "(cn=${email})",
   );
-  warn($mesg->error),
+  error($mesg->error),
   return undef if $mesg->code;
   my $entry = $mesg->shift_entry;
   return undef unless $entry;
