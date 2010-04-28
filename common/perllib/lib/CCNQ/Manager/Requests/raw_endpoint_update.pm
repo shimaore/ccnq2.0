b04593c3bc8429c9a5740194234a9903ce130a2b
Stephane Alnet <stephane@shimaore.net>
2010-04-28 18:17:54 +0200

    Re-introduced the raw (no provisioning/billing db updates) versions of the endpoint requests; they are needed to configure the inbound-proxy

---
+++ common/perllib/lib/CCNQ/Manager/Requests/raw_endpoint_update.pm
@@ -0,0 +1,31 @@
+package CCNQ::Manager::Requests::raw_endpoint_update;
+# Copyright (C) 2009  Stephane Alnet
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use CCNQ::Activities::Proxy;
+use CCNQ::Activities::Provisioning;
+use CCNQ::Activities::Billing;
+use CCNQ::Manager;
+
+sub run {
+  my $request = shift;
+  # Return list of activities required to complete this request.
+  return (
+    CCNQ::Activities::Proxy::endpoint_update($request),
+    CCNQ::Manager::request_completed(),
+  );
+}
+
+'CCNQ::Manager::Requests::raw_endpoint_update';
