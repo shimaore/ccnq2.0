69f6689c44dc7578ca5ebf67aa6a1d0003cf11fc
Stephane Alnet <stephane@shimaore.net>
2010-05-05 14:24:19 +0200

    be really nice

--- common/perllib/lib/CCNQ/B2BUA.pm
+++ common/perllib/lib/CCNQ/B2BUA.pm
@@ -80,7 +80,7 @@ sub crontab_update {
   my $freeswitch_username = 'freeswitch';
   my $crontab_line = <<CRON;
 SHELL=/bin/bash
-0 * * * *   nice ccnq2_crontab_cdr
+0 * * * *   nice -n 20 ccnq2_crontab_cdr
 CRON
   my $crontab_file = File::Spec->catfile(CCNQ::CCN,'ccnq2_crontab_cdr.crontab');
 
