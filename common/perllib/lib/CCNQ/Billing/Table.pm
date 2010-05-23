a69548db256d6e29893010862673a6843a18f0bf
Stephane Alnet <stephane@shimaore.net>
2010-05-23 22:37:58 +0200

    Properly map the names

--- common/perllib/lib/CCNQ/Billing/Table.pm
+++ common/perllib/lib/CCNQ/Billing/Table.pm
@@ -37,7 +37,7 @@ sub all_tables {
   my $couch = couch(CCNQ::Billing::billing_uri);
   $couch->all_dbs->cb(sub{
     my $dbs = CCNQ::AE::receive(@_);
-    $rcv->send( $dbs && [ map { s/^table_// } grep { /^table_/ } @$dbs ]);
+    $rcv->send( $dbs && [ map { s/^table_(.*)$/$1/ } grep { /^table_/ } @$dbs ]);
   });
   return $rcv;
 }
