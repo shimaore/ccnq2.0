11efaefe7df852e188ee002772d42f05410fc9c0
Stephane Alnet <stephane@shimaore.net>
2010-05-31 13:06:13 +0200

    Use Dancer s notion of prefix rather than a variable

--- portal/perllib/lib/CCNQ/Portal/Util.pm
+++ portal/perllib/lib/CCNQ/Portal/Util.pm
@@ -42,7 +42,7 @@ sub redirect_request {
   my $r = CCNQ::AE::receive(@_);
 
   # Redirect to the request
-  my $prefix = vars->{prefix};
+  my $prefix = prefix;
   defined $prefix or $prefix = '';
   my $url = $prefix.'/request/'.$r->{request};
   return redirect $url;
