4f73b92bfa63570e4c04b32d518113b9e26e40c3
Stephane Alnet <stephane@shimaore.net>
2010-07-25 17:13:26 +0200

    tab to spaces

--- portal/perllib/share/public/js/plan.js
+++ portal/perllib/share/public/js/plan.js
@@ -1,31 +1,31 @@
 $(function() {
-	$(".plan-guard").draggable({ revert: true });
-	$(".plan-action").draggable({ revert: true });
+  $(".plan-guard").draggable({ revert: true });
+  $(".plan-action").draggable({ revert: true });
 
-	$(".step-guard").droppable({
-	  accept: '.plan-guard',
-	  activeClass: 'ui-state-hover',
-	  hoverClass: 'ui-state-hover',
-		drop: function(event, ui) {
+  $(".step-guard").droppable({
+    accept: '.plan-guard',
+    activeClass: 'ui-state-hover',
+    hoverClass: 'ui-state-hover',
+    drop: function(event, ui) {
       var d = ui['draggable'];
       $(this).children('ul').append('<li>'+d.html()+'</li>');
-		}
-	});
+    }
+  });
 
-	$(".step-action").droppable({
-	  accept: '.plan-action',
-	  activeClass: 'ui-state-hover',
-	  hoverClass: 'ui-state-hover',
-		drop: function(event, ui) {
+  $(".step-action").droppable({
+    accept: '.plan-action',
+    activeClass: 'ui-state-hover',
+    hoverClass: 'ui-state-hover',
+    drop: function(event, ui) {
       var d = ui['draggable'];
       $(this).children('ul').append('<li>'+d.html()+'</li>');
-		}
-	});
+    }
+  });
 
-	$(".items").sortable({ placeholder: 'ui-state-highlight' });
+  $(".items").sortable({ placeholder: 'ui-state-highlight' });
 
   $("#plan").sortable({
-	  placeholder: 'ui-state-highlight'
+    placeholder: 'ui-state-highlight'
   });
 
 });
