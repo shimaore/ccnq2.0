161204c46bc9760ff139c890fd3d87c374a33bed
Stephane Alnet <stephane@shimaore.net>
2010-07-25 20:07:09 +0200

    prevent jquery running amok on large lists

--- portal/perllib/share/public/js/forms.js
+++ portal/perllib/share/public/js/forms.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
         $("form.validate").validate();
 
-        $("div.provisioning:lt(50)").accordion();
+        $("div.provisioning").slice(0,50).accordion();
         $("div.progressbar").progressbar();
 });
