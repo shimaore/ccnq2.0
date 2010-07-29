cc3dde9afce9d32ed99ac4a1f0b8b983bd1f402b
Stephane Alnet <stephane@shimaore.net>
2010-07-29 22:27:17 +0200

    Only one instance per server

--- common/perllib/lib/CCNQ/Actions/db/billing.pm
+++ common/perllib/lib/CCNQ/Actions/db/billing.pm
@@ -22,8 +22,16 @@ sub _install {
   return CCNQ::Billing::install(@_);
 }
 
+# Only one instance is needed per system.
+my $billing_room_done = 0;
+
 sub _session_ready {
   my ($params,$context) = @_;
+
+  return if $billing_room_done;
+
+  $billing_room_done = 1;
+
   use CCNQ::XMPPAgent;
   CCNQ::XMPPAgent::_join_room($context,CCNQ::Billing::billing_cluster_jid);
   return;
