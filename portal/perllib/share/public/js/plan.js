c2976a7fda4808d01285b5a7605fa592c568d517
Stephane Alnet <stephane@shimaore.net>
2010-08-03 16:25:18 +0200

    provide default target if none is available

--- portal/perllib/share/public/js/plan.js
+++ portal/perllib/share/public/js/plan.js
@@ -38,7 +38,7 @@ $(function() {
     var actions_holder = '<div class="step-action ui-widget-header"><p>Actions</p><ul class="items"></ul></div>';
     var step_holder    = '<li>'+guards_holder+actions_holder+'</li>';
 
-    for (step in data.rating_steps) {
+    for (step in data.rating_steps || []) {
       $("#plan").append(step_holder);
       var guard;
       for (guard  in step.guards) {
