61234c5b2d7b346ae4a01a074f6d225da99077a3
Stephane Alnet <stephane@shimaore.net>
2010-08-04 00:35:49 +0200

    highlight

--- portal/perllib/share/public/js/plan.js
+++ portal/perllib/share/public/js/plan.js
@@ -38,7 +38,7 @@ $(function() {
   var plan_name  = $("#plan_name").val();
 
   var remove_button  = '<span class="remove"><span title="Remove" class="ui-icon ui-icon-circle-minus"></span></span>';
-  var step_header    = '<div class="step-header ui-widget-header">'+remove_button+' Step</div>';
+  var step_header    = '<div class="step-header ui-widget-header">Step</div>'+remove_button;
   var guards_holder  = '<div class="step-guard ui-widget-header"><p>Guards</p><ul class="items"></ul></div>';
   var actions_holder = '<div class="step-action ui-widget-header"><p>Actions</p><ul class="items"></ul></div>';
   var step_holder    = '<li>'+step_header+guards_holder+actions_holder+'</li>';
