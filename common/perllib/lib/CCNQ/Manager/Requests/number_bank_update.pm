43402e9d7264ebeb6140227fcfc105973814f40a
Stephane Alnet <stephane@shimaore.net>
2010-08-10 15:21:45 +0200

    improper names

--- common/perllib/lib/CCNQ/Manager/Requests/number_bank_update.pm
+++ common/perllib/lib/CCNQ/Manager/Requests/number_bank_update.pm
@@ -15,6 +15,6 @@ package CCNQ::Manager::Requests::number_bank_update;
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use CCNQ::Activities::Provisioning;
-sub run { CCNQ::Activities::Provisioning->update_number(@_) }
+sub run { CCNQ::Activities::Provisioning::update_number(@_) }
 
 'CCNQ::Manager::Requests::number_bank_update';
