afa32868ce178d60b8a441e47edca5c29ff5a3f6
Stephane Alnet <stephane@shimaore.net>
2010-10-11 16:11:05 +0200

    bump version to 0.55

--- portal/perllib/lib/CCNQ/Invoice.pm
+++ portal/perllib/lib/CCNQ/Invoice.pm
@@ -9,23 +9,23 @@ package CCNQ::Invoice;
 Copyright (C) 2010  Stephane Alnet
 
 This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
+modify it under the terms of the GNU Affero General Public License
 as published by the Free Software Foundation; either version 3
 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 
 use 5.008;
 
-our $VERSION = '0.054';
+our $VERSION = '0.055';
 
 use strict; use warnings;
 
