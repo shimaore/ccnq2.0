3cd2f2195cf9250c6b5a5e0bb0d5ccd1c67a6fd5
Stephane Alnet <stephane@shimaore.net>
2010-10-19 23:56:32 +0200

    actually specify defaults

--- portal/perllib/share/public/js/paginate.js
+++ portal/perllib/share/public/js/paginate.js
@@ -1,8 +1,8 @@
 $(function() {
   var refresh;
   refresh = function(view,page_offset) {
-    var current_page  = view.children(".current_page").html();
-    var limit         = view.children(".per_page").val();
+    var current_page  = view.children(".current_page").html() || 1;
+    var limit         = view.children(".per_page").val()      || 25;
 
     var new_page = current_page + page_offset;
     var url = "?page="+new_page+"&limit="+limit;
