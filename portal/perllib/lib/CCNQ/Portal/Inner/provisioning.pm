e908d9b6d6005574277bb0d3fb08b674c60f9e09
Stephane Alnet <stephane@shimaore.net>
2010-10-20 01:59:49 +0200

    now needs parameters
    no changes added to commit (use "git add" and/or "git commit -a")

--- portal/perllib/lib/CCNQ/Portal/Inner/provisioning.pm
+++ portal/perllib/lib/CCNQ/Portal/Inner/provisioning.pm
@@ -121,8 +121,7 @@ sub paginate_html {
   var limit  => $limit;
   var result => sub { CCNQ::AE::receive_docs($cv) };
 
-  var template_name => 'provisioning-paginate';
-  template 'provisioning-paginate', {}, { layout => undef };
+  template 'provisioning-paginate', vars, { layout => undef };
 }
 
 sub _view_page {
