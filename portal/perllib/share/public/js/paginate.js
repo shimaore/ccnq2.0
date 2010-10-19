3d54488b344d5a7d2a645fb6ecaa80c1585add9c
Stephane Alnet <stephane@shimaore.net>
2010-10-20 00:50:13 +0200

    explicit number conversions; correct formula for same_page

--- portal/perllib/share/public/js/paginate.js
+++ portal/perllib/share/public/js/paginate.js
@@ -1,8 +1,8 @@
 $(function() {
   var refresh;
   refresh = function(view,page_offset) {
-    var current_page  = view.find(".current_page").html() || 1;
-    var limit         = view.find(".per_page").val()      || 25;
+    var current_page  = 0 + view.find(".current_page").html() || 1;
+    var limit         = 0 + view.find(".per_page").val()      || 25;
 
     var new_page = current_page + page_offset;
     var url = "?page="+new_page+"&limit="+limit;
@@ -17,9 +17,9 @@ $(function() {
         return;
       });
       view.find(".per_page").change(function(){
-        var new_limit = view.find(".per_page").val();
-        var same_page = (new_page / limit) * new_limit;
-        refresh(view,same_page-current_page);
+        var new_limit = 0 + view.find(".per_page").val();
+        var same_page = Math.floor((new_page * limit) / new_limit);
+        refresh(view,same_page.-current_page);
       });
     });
   };
