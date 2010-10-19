e4e01e273ad75760a6df665211d31d65c6fb8359
Stephane Alnet <stephane@shimaore.net>
2010-10-20 00:45:48 +0200

    more children()

--- portal/perllib/share/public/js/paginate.js
+++ portal/perllib/share/public/js/paginate.js
@@ -1,8 +1,8 @@
 $(function() {
   var refresh;
   refresh = function(view,page_offset) {
-    var current_page  = view.children(".current_page").html() || 1;
-    var limit         = view.children(".per_page").val()      || 25;
+    var current_page  = view.find(".current_page").html() || 1;
+    var limit         = view.find(".per_page").val()      || 25;
 
     var new_page = current_page + page_offset;
     var url = "?page="+new_page+"&limit="+limit;
@@ -10,12 +10,14 @@ $(function() {
     view.find(".view_content").load(url, function(){
       view.find(".prev_page").click(function(){
         refresh(view,-1);
+        return;
       });
       view.find(".next_page").click(function(){
         refresh(view,+1);
+        return;
       });
       view.find(".per_page").change(function(){
-        var new_limit = view.children(".per_page").val();
+        var new_limit = view.find(".per_page").val();
         var same_page = (new_page / limit) * new_limit;
         refresh(view,same_page-current_page);
       });
