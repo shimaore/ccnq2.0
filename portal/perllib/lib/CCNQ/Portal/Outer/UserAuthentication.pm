95f13d3cd2e8a1aa4f57af2c373811f0819b478e
Stephane Alnet <stephane@shimaore.net>
2011-03-14 15:12:31 +0100

    Dancer prefix migth be undef

--- portal/perllib/lib/CCNQ/Portal/Outer/UserAuthentication.pm
+++ portal/perllib/lib/CCNQ/Portal/Outer/UserAuthentication.pm
@@ -29,12 +29,16 @@ post '/login' => sub {
       CCNQ::Portal::Outer::AccountSelection::account();
     }
   );
-  redirect uri_for(prefix.'/');
+  my $prefix = prefix;
+  defined $prefix or $prefix = '';
+  redirect uri_for($prefix.'/');
 };
 
 get '/logout' => sub {
   CCNQ::Portal->current_session->end();
-  redirect uri_for(prefix.'/');
+  my $prefix = prefix;
+  defined $prefix or $prefix = '';
+  redirect uri_for($prefix.'/');
 };
 
 'CCNQ::Portal::Outer::UserAuthentication';
